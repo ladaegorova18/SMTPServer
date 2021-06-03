be3772e7ee057bbd23895d3ae5a348228b5d2ef7
ladaegorova18 <st068843@student.spbu.ru>
2021-06-03 14:34:45 +0300

    added new thread to client

--- client.py
+++ client.py
@@ -1,25 +1,39 @@
 import socket
+import sys
+from threading import Thread
 
 port = 25
 
 # СоздаемTCP/IP сокет
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+# 84.252.138.83
 # Подключаем сокет к порту, через который прослушивается сервер
 server_address = ('localhost', port)
-print('Connected to {} port {}'.format(*server_address))
 sock.connect(server_address)
+print('Connected to {} port {}'.format(*server_address))
+data = sock.recv(512)
+answer = data.decode().replace('\n', '')
+print(f'Received: {answer}')
+
+def awaitAnswer():
+    # Смотрим ответ
+    data = sock.recv(512)
+    answer = data.decode().replace('\n', '')
+    print(f'Received: {answer}')
+
 
 try:
     while True:
-        # Смотрим ответ
-        data = sock.recv(512)
-        print(f'Received: {data.decode()}')
-
         # Отправка данных
-        mess = input()
+        mess = input().split("\n")
+        if mess != '' and mess != '\n':
+            for line in mess:
+                sock.sendall(line.encode())
+
+
         print(f'Sent: {mess}')
-        sock.send(mess.encode())
+
+        Thread(target=awaitAnswer()).start()
 
 finally:
     print('Closing...')
--- server1.py
+++ server1.py
@@ -2,6 +2,7 @@ import socket
 import os
 import sys
 from datetime import datetime
+from threading import Thread
 
 port = 25
 
@@ -15,6 +16,59 @@ sock.bind(server_address)
 
 # Слушаем входящие подключения
 sock.listen(1)
+
+
+def clientHandler(client, count):
+    sender_name = ""
+    sender_mail = ""
+    address_name = ""
+    info = ""
+    text = ""
+    mess = "Welcome to the SMTP server! \n"
+    connection.send(mess.encode())
+    # Принимаем данные порциями и ретранслируем их
+    try:
+        while True:
+            message = connection.recv(512).decode()
+            print(f'Получено: {message}')
+
+            if message.startswith("QUIT"):
+                mess = "221 Bye!"
+                connection.send(mess.encode())
+                break
+
+            elif message.startswith("HELO") :
+                sender_name = message[4:]  # обрезать
+                mess = "250 OK"
+
+            elif message.startswith("MAIL FROM"):
+                sender_mail = message[10:]
+                mess = "250 OK"
+
+            elif message.startswith("RCPT TO"):
+                address_name = message[8:]
+                mess = "250 OK"
+
+            elif message.startswith("DATA"):
+                info = message[4:]
+                mess = "354 Send message content"
+                connection.send(mess.encode())
+                data = connection.recv(4096)
+                text = data.decode()
+                mess = "250 OK"
+                storeLetter(sender_mail, sender_name, address_name, info, text, count)
+                count += 1
+
+            else:
+                mess = "Invalid data"
+            connection.send(mess.encode())
+
+    except Exception:
+        print("Exception occured")
+
+    finally:
+        connection.close()
+
 
 def storeLetter(sender_mail, sender_name, address_name, info, text, count):
     letter = ""
@@ -34,59 +88,15 @@ def storeLetter(sender_mail, sender_name, address_name, info, text, count):
     with open(name, "w") as file:
         file.write(letter)
 
+
 while True:
-    # ждем соединения
-    print('Waiting for requests...')
-    count = 0
-    connection, client_address = sock.accept()
     try:
+        # ждем соединения
+        print('Waiting for requests...')
+        count = 0
+        connection, client_address = sock.accept()
         print('Connected to:', client_address)
-        sender_name = ""
-        sender_mail = ""
-        address_name = ""
-        info = ""
-        text = ""
-        mess = "Welcome to the SMTP server!"
-        connection.send(mess.encode())
-        # Принимаем данные порциями и ретранслируем их
-        while True:
-            message = connection.recv(512).decode()
-            print(f'Получено: {message}')
-
-            if message.startswith("QUIT"):
-                mess = "221 Bye!"
-                connection.send(mess.encode())
-                break
-
-            elif message.startswith("HELO"):
-                sender_name = message[4:] #обрезать
-                mess = "250 OK"
-                connection.send(mess.encode())
-
-            elif message.startswith("MAIL FROM"):
-                sender_mail = message[10:]
-                mess = "250 OK"
-                connection.send(mess.encode())
-
-            elif message.startswith("RCPT TO"):
-                address_name = message[8:]
-                mess = "250 OK"
-                connection.send(mess.encode())
-
-            elif message.startswith("DATA"):
-                info = message[4:]
-                mess = "354 Send message content"
-                connection.send(mess.encode())
-                data = connection.recv(4096)
-                text = data.decode()
-                mess = "250 OK"
-                connection.send(mess.encode())
-                storeLetter(sender_mail, sender_name, address_name, info, text, count)
-                count += 1
-
-            else:
-                print('No data from:', client_address)
-
+        Thread(target=clientHandler(connection, count)).start()
     finally:
         # Очищаем соединение
         connection.close()
